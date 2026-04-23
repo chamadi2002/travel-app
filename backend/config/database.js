766ec852300326678e2d38fd0c8fa1913fd440f5
Chamadi Weerasinghe <116907681+chamadi2002@users.noreply.github.com>
2026-04-23 10:58:39 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,11 +5,10 @@ dotenv.config();
 
 const pool = mysql.createPool({
     host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
-      
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
